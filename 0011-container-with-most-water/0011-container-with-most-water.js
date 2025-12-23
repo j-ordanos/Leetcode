c3a311c22c72a9fb01d35d5c06bbc02249cc713b
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2025-12-23 10:34:16 +0300

    Time: 2 ms (79.14%), Space: 62.8 MB (73.63%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+
+    while(left < right){
+
+        let width = right - left;
+        let maxArea = Math.min(height[left], height[right]) * width;
+        max = Math.max(maxArea, max);
+
+        if(height[left] <= height[right]){
+            left++;
+        }else{
+            right--;
+        }
+    }
+
+    return max;
+};
