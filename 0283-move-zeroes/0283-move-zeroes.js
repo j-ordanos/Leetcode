fa2f1d0fd7e0b48bba2729431f92add2044e0055
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-01-02 00:19:42 +0300

    Time: 0 ms (100%), Space: 61 MB (22.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -7,7 +7,7 @@ var moveZeroes = function(nums) {
     for(let i = 0; i < nums.length; i++){
         if(nums[i] !== 0){
             nums[k] = nums[i];
-            if(k != i) nums[i] = 0;
+            if(k !== i) nums[i] = 0;
             k++;
         }
     }
