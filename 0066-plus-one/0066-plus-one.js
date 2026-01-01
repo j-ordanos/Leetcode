13141b65af2a1863d9881b1480aff5ba98c13296
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-01-02 01:05:59 +0300

    Time: 0 ms (100%), Space: 52.4 MB (98.64%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,6 +3,15 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    let num = BigInt(digits.join('')) + 1n;
-    return num.toString().split('').map(Number);
+    for(let i = digits.length -1; i >= 0; i--){
+        if(digits[i] < 9){
+          digits[i]++;
+           return digits;
+        }
+
+        digits[i] = 0;
+    }
+
+    digits.unshift(1);
+    return digits;
 };
