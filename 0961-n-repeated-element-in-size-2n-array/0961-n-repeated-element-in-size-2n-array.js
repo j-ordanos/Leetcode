4c3e5f33cf3cb4798d6657e0db30fb8967ba4f08
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-01-02 15:24:54 +0300

    Time: 0 ms (100%), Space: 57.1 MB (71.43%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.js
@@ -3,7 +3,10 @@
  * @return {number}
  */
 var repeatedNTimes = function(nums) {
-   return nums.find((elt, idx, arr) =>{
-        return arr.indexOf(elt) < idx;
-    }); 
+    let n = nums.length
+   for(let i = 0; i < n; i++){
+        if(i+1 < n && nums[i] === nums[i+1]) return nums[i];
+        if(i+1 < n && nums[i] === nums[i+2]) return nums[i];
+        if(i+1 < n && nums[i] === nums[i+3]) return nums[i];
+   }
 };
