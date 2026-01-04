adb0cb6ef9f561332dce72f2317a620d8ef25ea0
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-01-04 10:51:48 +0300

    Time: 14 ms (76.47%), Space: 56.6 MB (76.47%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.js
+++ 1390-four-divisors/1390-four-divisors.js
@@ -6,17 +6,29 @@ var sumFourDivisors = function(nums) {
     let total_sum = 0;
 
     for(let num of nums){
-        let divisors = [];
-        for(let i = 1; i <= num; i++){
-            if(num % i === 0){
-                divisors.push(i);
-            }
+        let sum = 0;
+        let count = 0;
 
-            if(divisors.length > 4) break;
-        }
-        if(divisors.length === 4){
-                total_sum += divisors[0] + divisors[1] + divisors[2] + divisors[3]; 
+        for(let i = 1; i*i <= num; i++){
+            if(num % i === 0){
+                let d1 = i;
+                let d2 = num / i;
+
+                if(d1 === d2){
+                    count += 1;
+                    sum += d1;
+                }else{
+                    count += 2;
+                    sum += d1 + d2;
+                }
+
+                if(count > 4) break;
             }
+        }
+
+        if(count === 4){
+            total_sum += sum;
+        }
     }
 
     return total_sum;
