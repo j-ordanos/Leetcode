36135ed2706825b05e435e4b57bcec9380d781e0
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-01-04 11:32:38 +0300

    Time: 240 ms (92.85%), Space: 96.2 MB (55.67%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -3,12 +3,13 @@
  * @return {Function}
  */
 function memoize(fn) {
-    const cache = {};
+    const cache = new Map();
     return function(...args) {
        let key = JSON.stringify(args);
-       if(key in cache) return cache[key];
-       cache[key] = fn(...args);
-       return cache[key]; 
+       if(cache.has(key)) return cache.get(key);
+       let result = fn(...args);
+       cache.set(key, result);
+       return result; 
     }
 }
 
