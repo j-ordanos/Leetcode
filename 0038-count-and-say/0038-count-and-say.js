ff2c3f9918efe20032730579ffc56efddac77195
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-01-10 00:40:09 +0300

    Time: 10 ms (5.42%), Space: 55.8 MB (74.58%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    if( n === 1) return '1';
+
+    let prev = countAndSay(n-1);
+    let result = '';
+    let count = 1;
+
+    for(let i = 1; i < prev.length; i++){
+        if(prev[i] === prev[i-1]){
+            count++;
+        }else{
+            result += count.toString() + prev[i-1];
+            count = 1;
+        }
+    }
+
+    result += count.toString() + prev[prev.length - 1];
+    return result;
+};
