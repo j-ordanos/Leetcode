d41480a092b29c9b9834a7c71b43328082a50924
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-01-11 10:42:05 +0300

    Time: 21 ms (47.16%), Space: 69.9 MB (53.4%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
@@ -3,37 +3,19 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
-    const n = heights.length;
-    const left = new Array(n);
-    const right = new Array(n);
     const stack = [];
-
-    // Fill left[]
-    for(let i=0; i<n; i++){
-        while(stack.length>0 && heights[stack[stack.length-1]] >= heights[i]){
-            stack.pop();
-        }
-        left[i] = stack.length === 0 ? 0 : stack[stack.length-1] + 1;
-        stack.push(i);
-    }
-
-    // clear stack
-    stack.length = 0;
-
-    // Fill right[]
-    for(let i=n-1; i>=0; i--){
-        while(stack.length>0 && heights[stack[stack.length-1]] >= heights[i]){
-            stack.pop();
-        }
-        right[i] = stack.length === 0 ? n-1 : stack[stack.length-1] - 1;
-        stack.push(i);
-    }
-
-    // calculate max area
     let maxArea = 0;
-    for(let i=0; i<n; i++){
-        maxArea = Math.max(maxArea, heights[i] * (right[i] - left[i] + 1));
-    }
 
+    // Add sentinel
+    heights.push(0);
+
+    for(let i=0; i<heights.length; i++){
+        while(stack.length >0 && heights[stack[stack.length-1]] > heights[i]){
+            const h = heights[stack.pop()];
+            const width = stack.length === 0 ? i : i-stack[stack.length-1] - 1;
+            maxArea = Math.max(maxArea, h*width);
+        }
+        stack.push(i);
+    }
     return maxArea;
 };
