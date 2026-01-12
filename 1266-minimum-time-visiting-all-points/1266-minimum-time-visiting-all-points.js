e52f6c5b34384f0922313c43ef35e40fbe84a7e9
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-01-12 09:38:15 +0300

    Time: 2 ms (26.88%), Space: 56.5 MB (27.96%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    let totalTime = 0;
+
+    for(let i=0; i<points.length-1; i++){
+        const [x1, y1] = points[i];
+        const [x2, y2] = points[i+1];
+
+        totalTime += Math.max(Math.abs(x2-x1), Math.abs(y2-y1));
+    }
+
+    return totalTime;
+};
