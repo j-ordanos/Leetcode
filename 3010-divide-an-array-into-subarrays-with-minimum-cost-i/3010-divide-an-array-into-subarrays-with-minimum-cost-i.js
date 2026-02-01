072c44875c537fcb08a8a94d5167215bb02e1405
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-02-01 13:57:12 +0300

    Time: 0 ms (100%), Space: 55.8 MB (80.77%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumCost = function(nums) {
+    let total_cost = nums[0];
+    let min1 = Infinity;
+    let min2 = Infinity;
+
+    for(let i=1; i<nums.length; i++){
+        if(nums[i] < min1){
+            min2 = min1;
+            min1 = nums[i];
+        }
+        else if(nums[i] < min2){
+            min2 = nums[i];
+        }
+    }
+
+    return total_cost + min1 + min2;
+};
