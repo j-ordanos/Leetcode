63bdcb74396077054cfa80f526dca63257b8c6ab
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-02-09 15:38:10 +0300

    Time: 6 ms (77.61%), Space: 74.5 MB (43.28%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var balanceBST = function(root) {
+    const result = [];
+
+    function traverse(node){
+        if(!node) return;
+
+        traverse(node.left);
+        result.push(node.val)
+        traverse(node.right);
+    }
+
+    function buildBST(left,right){
+        if(left > right) return null;
+        
+        let mid = Math.floor((left + right)/2);
+        let node = new TreeNode(result[mid]);
+        node.left = buildBST(left, mid-1);
+        node.right = buildBST(mid+1, right);
+        
+        return node;
+    }
+
+    traverse(root);
+    return buildBST(0, result.length-1);
+
+    
+};
