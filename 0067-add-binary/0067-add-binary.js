07622d7ef92d1f71bc0bf7caacd7aca6d4b21c3e
Yordanos <151696220+j-ordanos@users.noreply.github.com>
2026-02-15 12:05:04 +0300

    Time: 1 ms (69.1%), Space: 55.6 MB (46.85%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,6 +4,21 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    const sum = BigInt(`0b${a}`) + BigInt(`0b${b}`);
-    return sum.toString(2);
+    let res = '';
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+
+    while(i>=0 || j>=0 || carry){
+        let bitA = i >= 0 ? Number(a[i]) : 0;
+        let bitB = j >= 0 ? Number(b[j]) : 0;
+
+        let sum = bitA + bitB + carry;
+        res = (sum % 2) + res;
+        carry = Math.floor(sum/2);
+
+        i--;
+        j--;
+    }
+    return res;
 };
