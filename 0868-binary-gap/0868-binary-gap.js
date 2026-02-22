e6968bf4c4d7f4e271758ff1736b1dd0dd3b86fa
Yordanos Zewge <151696220+j-ordanos@users.noreply.github.com>
2026-02-22 22:49:37 +0300

    Time: 1 ms (30.56%), Space: 54 MB (50%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.js
+++ 0868-binary-gap/0868-binary-gap.js
@@ -2,26 +2,18 @@
  * @param {number} n
  * @return {number}
  */
-
-function getGaps(binaryArr, gaps){
-    const first = binaryArr.indexOf('1');
-    if (first === -1) return (gaps.length > 0) ? Math.max(...gaps) : 0;
-
-    let remaining = binaryArr.slice(first + 1);
-    const second = remaining.indexOf('1');
-
-    if (second > -1) {
-        gaps.push(second + 1);
-        return getGaps(remaining.slice(second), gaps);
-    }
-
-    return (gaps.length > 0) ? Math.max(...gaps) : 0;
-}
-
 var binaryGap = function(n) {
-    if(n === parseInt(n, 10) && n >= 1 && n <= 2147483647){
-        const binary = n.toString(2).split('');
-        return getGaps(binary, []);
+    let binary = n.toString(2);
+    let maxGap = 0;
+    let last = -1;
+
+    for (let i = 0; i < binary.length; i++) {
+        if (binary[i] === '1') {
+            if (last !== -1) {
+                maxGap = Math.max(maxGap, i - last);
+            }
+            last = i;
+        }
     }
-    return 0;
+    return maxGap;
 };
