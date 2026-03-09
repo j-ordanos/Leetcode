6ba92acb8ece8cc359ad7b1ba6e107f2434beca4
Yordanos Zewge <151696220+j-ordanos@users.noreply.github.com>
2026-03-09 22:10:31 +0300

    Time: 1 ms (85.3%), Space: 58 MB (57.02%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.js
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.js
@@ -11,13 +11,17 @@
  * @return {TreeNode}
  */
 var sortedArrayToBST = function(nums) {
-    if (!nums.length) return null;
+    function buildBST(left, right) {
+        if (left > right) return null;
 
-    const mid = Math.floor(nums.length / 2);
-    const root = new TreeNode(nums[mid]);
+        const mid = Math.floor((left + right) / 2);
+        const node = new TreeNode(nums[mid]);
 
-    root.left = sortedArrayToBST(nums.slice(0, mid));
-    root.right = sortedArrayToBST(nums.slice(mid + 1));
+        node.left = buildBST(left, mid - 1);
+        node.right = buildBST(mid + 1, right);
 
-    return root;
+        return node;
+    }
+
+    return buildBST(0, nums.length - 1);
 };
