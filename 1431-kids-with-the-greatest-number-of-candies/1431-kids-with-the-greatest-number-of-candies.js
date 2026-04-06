88ec7ccfcc186b1d041080ed19a6516caf8806d8
Yordanos Zewge <151696220+j-ordanos@users.noreply.github.com>
2026-04-06 23:47:19 +0300

    Time: 0 ms (100%), Space: 56.1 MB (12.03%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const max = Math.max(...candies);
+    let res = [];
+
+    for(let i=0; i<candies.length; i++){
+        res.push(candies[i] + extraCandies >= max);
+    }
+    return res;
+};
