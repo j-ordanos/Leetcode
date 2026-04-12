fc374c7c4d772a46b4a1f381eca98f915cbf995d
Yordanos Zewge <151696220+j-ordanos@users.noreply.github.com>
2026-04-12 22:22:43 +0300

    Time: 2 ms (32.23%), Space: 55.2 MB (26.24%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(operations) {
+    let stack = [];
+    for (let op of operations) {
+        if (op === '+') {
+            stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+        } else if (op === 'D') {
+            stack.push(2 * stack[stack.length - 1]);
+        } else if (op === 'C') {
+            stack.pop();
+        } else {
+            stack.push(parseInt(op));
+        }
+    }
+    return stack.reduce((a, b) => a + b, 0);
+};
