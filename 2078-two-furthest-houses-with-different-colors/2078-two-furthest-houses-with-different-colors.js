ff7686cbc1e85e864c3526544d5ee38f386d55e0
Yordanos Zewge <151696220+j-ordanos@users.noreply.github.com>
2026-04-20 23:54:27 +0300

    Time: 0 ms (100%), Space: 53 MB (92.31%) - LeetHub

---
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} colors
+ * @return {number}
+ */
+var maxDistance = function(colors) {
+    let n = colors.length;
+    let maxDist = 0;
+
+    for (let i = n - 1; i >= 0; i--) {
+        if (colors[i] !== colors[0]) {
+            maxDist = Math.max(maxDist, i);
+            break;
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (colors[i] !== colors[n - 1]) {
+            maxDist = Math.max(maxDist, (n - 1) - i);
+            break;
+        }
+    }
+
+    return maxDist;
+};
